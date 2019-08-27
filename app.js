8fa14935786103af13ea1f6c36e0a45f7750c260
GarenWang <2270099149@qq.com>
2019-08-27 23:12:30 +0800

    什么是服务器渲染

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+       <html lang="en">
+        <title>Hello world</title>
+        <body>
+            <h1>Hello World</h1>
+        </body>
+       </html>
+    `)
+});
+
+app.listen(8000, () => {
+    console.log("server is start at localhost:8000")
+});
